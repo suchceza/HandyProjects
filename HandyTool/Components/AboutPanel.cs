a965c484228587fcdf61dc9c354c6a42c701931f
suchceza <suchceza@gmail.com>
2019-05-18 02:14:38 +0200

    adjusting about panel

--- HandyTool/Components/AboutPanel.cs
+++ HandyTool/Components/AboutPanel.cs
@@ -30,10 +30,21 @@ namespace HandyTool.Components
             Name = "AboutPanel";
             TabIndex = 0;
             TabStop = false;
-            Size = new Size(ParentControl.Width - 2, ParentControl.Width - 2);
-            Location = new Point(0, 0);
+            Location = new Point(1, 1);
             BackColor = Color.Yellow;
             Visible = false;
+            VisibleChanged += AboutPanel_VisibleChanged;
+            Click += AboutPanel_Click;
+        }
+
+        private void AboutPanel_Click(object sender, System.EventArgs e)
+        {
+            Visible = false;
+        }
+
+        private void AboutPanel_VisibleChanged(object sender, System.EventArgs e)
+        {
+            Size = new Size(ParentControl.Width - 2, ParentControl.Height - 2);
         }
 
         #endregion
